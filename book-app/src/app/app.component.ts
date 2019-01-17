6b8ebb35f04f09ad4a3f1fdcc0290d06f6844ea9
OiseauPython <stankojeremy96@gmail.com>
2019-01-17 21:51:52 +0100

    add header and firebase

--- book-app/src/app/app.component.ts
+++ book-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'book-app';
+  	constructor() {
+		const config = {
+			apiKey: "AIzaSyBgBs1GyO0DWJlSk9OtfjAD0YPpPcbAECs",
+			authDomain: "book-app-d8837.firebaseapp.com",
+			databaseURL: "https://book-app-d8837.firebaseio.com",
+			projectId: "book-app-d8837",
+			storageBucket: "",
+			messagingSenderId: "132566906554"
+		};
+		firebase.initializeApp(config);
+	}
 }
